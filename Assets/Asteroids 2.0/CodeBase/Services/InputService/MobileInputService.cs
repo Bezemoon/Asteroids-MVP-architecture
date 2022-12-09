e3a92299916356c116beae162ee80f6012298497
Bezemoon <aybolkabiev@gmail.com>
2022-12-09 13:40:27 +0600

    Add Input Service

--- Assets/Asteroids 2.0/CodeBase/Services/InputService/MobileInputService.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-namespace Asteroids_2._0.CodeBase.Services.InputService
-{
-    public class MobileInputService: InputService
-    { 
-        public override Vector2 Axis => SimpleInputAxis();
-    }
-}
