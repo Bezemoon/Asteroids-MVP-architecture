8537d31e7cbe84e526df8d22cad1548b8b6dcfbc
Bezemoon <aybolkabiev@gmail.com>
2022-12-09 13:47:20 +0600

    Added namespaces and removed unused namespaces

--- Assets/Asteroids LegacyVersion/Scripts/Asteroids/AsteroidSpawn.cs
+++ Assets/Asteroids LegacyVersion/Scripts/Asteroids/AsteroidSpawn.cs
@@ -1,33 +1,35 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AsteroidSpawn : MonoBehaviour
+namespace Asteroids_LegacyVersion.Scripts.Asteroids
 {
-    [SerializeField] private List<Asteroid> _asteroidPrefabs;
-    [SerializeField] private int _quantity;
-    [SerializeField] private float _maxValueSpawnPoint;
-    [SerializeField] private float _minValueSpawnPoint;
-
-    private Camera _camera;
-    private int _currentQuantity;
-
-    private void Awake()
+    public class AsteroidSpawn : MonoBehaviour
     {
-        _camera = GetComponent<Camera>();
-        _currentQuantity = _quantity;
-        Spawn();
-    }
+        [SerializeField] private List<Asteroid> _asteroidPrefabs;
+        [SerializeField] private int _quantity;
+        [SerializeField] private float _maxValueSpawnPoint;
+        [SerializeField] private float _minValueSpawnPoint;
 
-    private void Spawn()
-    {
-        for(int i = 0; i < _quantity; i++)
+        private Camera _camera;
+        private int _currentQuantity;
+
+        private void Awake()
         {
-            int asteroidXPosition = (int) Random.Range(_minValueSpawnPoint, _maxValueSpawnPoint);
-            int asteroidYPosition = (int) Random.Range(_minValueSpawnPoint, _maxValueSpawnPoint);
+            _camera = GetComponent<Camera>();
+            _currentQuantity = _quantity;
+            Spawn();
+        }
 
-            var asteroid = Instantiate(_asteroidPrefabs[Random.Range(0, _asteroidPrefabs.Count)], new Vector3(asteroidXPosition, asteroidYPosition, 0), Quaternion.identity);
-            asteroid.SetDirection(Random.Range(0, 360));
+        private void Spawn()
+        {
+            for(int i = 0; i < _quantity; i++)
+            {
+                int asteroidXPosition = (int) Random.Range(_minValueSpawnPoint, _maxValueSpawnPoint);
+                int asteroidYPosition = (int) Random.Range(_minValueSpawnPoint, _maxValueSpawnPoint);
+
+                var asteroid = Instantiate(_asteroidPrefabs[Random.Range(0, _asteroidPrefabs.Count)], new Vector3(asteroidXPosition, asteroidYPosition, 0), Quaternion.identity);
+                asteroid.SetDirection(Random.Range(0, 360));
+            }
         }
     }
 }
