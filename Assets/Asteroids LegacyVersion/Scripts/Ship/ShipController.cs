70b8f7a58d305e2998af5cd7e9462f1191e0fc37
Bezemoon <aybolkabiev@gmail.com>
2022-12-09 13:54:52 +0600

    Added namespace and removed unused namespaces

--- Assets/Asteroids LegacyVersion/Scripts/Ship/ShipController.cs
+++ Assets/Asteroids LegacyVersion/Scripts/Ship/ShipController.cs
@@ -1,63 +1,65 @@
-using Asteroids_1._0.Scripts.Ship;
 using UnityEngine;
 
-public class ShipController : MonoBehaviour
+namespace Asteroids_LegacyVersion.Scripts.Ship
 {
-    [SerializeField] private Ship _ship;
-    [SerializeField] private ShipMovement _movement;
-
-    private void Update()
+    public class ShipController : MonoBehaviour
     {
-        if (!_movement.IsStopped)
+        [SerializeField] private Ship _ship;
+        [SerializeField] private ShipMovement _movement;
+
+        private void Update()
         {
-            if (!_ship.IsReloading)
+            if (!_movement.IsStopped)
             {
-                if (Input.GetMouseButtonDown(0))
+                if (!_ship.IsReloading)
                 {
-                    if (_ship.CurrentWeapon.TryGetComponent(out Laser laser))
+                    if (Input.GetMouseButtonDown(0))
                     {
-                        _movement.StopMotion();
-                    }
+                        if (_ship.CurrentWeapon.TryGetComponent(out Laser laser))
+                        {
+                            _movement.StopMotion();
+                        }
 
-                    if (_ship.CurrentWeapon.CurrentCapacity > 0)
-                    {
+                        if (_ship.CurrentWeapon.CurrentCapacity > 0)
+                        {
 
-                        _ship.CurrentWeapon.Shoot(_ship.ShootPoint);
+                            _ship.CurrentWeapon.Shoot(_ship.ShootPoint);
 
-                    }
-                    else
-                    {
-                        _ship.ChangeReloadingState();
+                        }
+                        else
+                        {
+                            _ship.ChangeReloadingState();
+                        }
                     }
                 }
-            }
+                else
+                {
+                    _ship.ReloadTimer();
+
+                }
+
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    _ship.SwitchWeapon();
+                }
+
+                if (Input.GetKey(KeyCode.A))
+                    _movement.Rotate(_movement.SpeedRotation);
+
+                if (Input.GetKey(KeyCode.D))
+                    _movement.Rotate(-_movement.SpeedRotation);
+
+                if (Input.GetKey(KeyCode.W))
+                    _movement.IncreaseValue();
+                else
+                    _movement.DecreaseValue();
+
+                _movement.Move();
+            } 
             else
             {
-                _ship.ReloadTimer();
-
+                _movement.Timer();
             }
-
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                _ship.SwitchWeapon();
-            }
-
-            if (Input.GetKey(KeyCode.A))
-                _movement.Rotate(_movement.SpeedRotation);
-
-            if (Input.GetKey(KeyCode.D))
-                _movement.Rotate(-_movement.SpeedRotation);
-
-            if (Input.GetKey(KeyCode.W))
-                _movement.IncreaseValue();
-            else
-                _movement.DecreaseValue();
-
-            _movement.Move();
-        } 
-        else
-        {
-            _movement.Timer();
         }
     }
 }
