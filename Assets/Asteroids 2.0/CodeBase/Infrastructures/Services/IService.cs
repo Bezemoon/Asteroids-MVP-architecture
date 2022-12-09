072b2a5886ae6fa85e418516ec37057d3204c3ec
Bezemoon <aybolkabiev@gmail.com>
2022-12-09 13:45:55 +0600

    Removed unused namespaces

--- Assets/Asteroids 2.0/CodeBase/Infrastructures/Services/IService.cs
+++
@@ -1,6 +0,0 @@
-namespace Asteroids_2._0.CodeBase.Infrastructures.Services
-{
-    public interface IService
-    {
-    }
-}
--- Assets/Asteroids LegacyVersion/Scripts/Asteroids/Asteroid.cs
+++ Assets/Asteroids LegacyVersion/Scripts/Asteroids/Asteroid.cs
@@ -1,58 +1,59 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class Asteroid : MonoBehaviour
+namespace Asteroids_LegacyVersion.Scripts.Asteroids
 {
-    [SerializeField] private float _speed;
-    [SerializeField] private bool _isShare;
-    [SerializeField] private Asteroid _fragmentPrefab;
-
-    private readonly int _numberOfFragments = 2;
-    private Vector3 _direction;
-
-    public bool IsShare
+    public class Asteroid : MonoBehaviour
     {
-        get { return _isShare; }
-        set { _isShare = value; }
-    }
+        [SerializeField] private float _speed;
+        [SerializeField] private bool _isShare;
+        [SerializeField] private Asteroid _fragmentPrefab;
 
-    private void Update()
-    {
-        transform.Translate(_direction * _speed * Time.deltaTime);
-    }
+        private readonly int _numberOfFragments = 2;
+        private Vector3 _direction;
 
-    public void Explode()
-    {
-        _speed = 0;
-
-        if (_isShare)
+        public bool IsShare
         {
-            BreakDown();
+            get { return _isShare; }
+            set { _isShare = value; }
         }
 
-        Destruction();
-    }
-
-    private void BreakDown()
-    {
-        for(int i = 0; i < _numberOfFragments; i++)
+        private void Update()
         {
-            var asteroid = Instantiate(_fragmentPrefab, transform.position, Quaternion.identity);
-
-            asteroid.SetDirection(transform.rotation.z + (Mathf.Pow(-1, i) * 15 * -1));
+            transform.Translate(_direction * _speed * Time.deltaTime);
         }
-    }
 
-    public void SetDirection(float angle)
-    {
-        float x = transform.position.magnitude * Mathf.Cos(Mathf.Deg2Rad * angle);
-        float y = transform.position.magnitude * Mathf.Sin(Mathf.Deg2Rad * angle);
-        _direction = new Vector3(x, y).normalized;
-    }
+        public void Explode()
+        {
+            _speed = 0;
 
-    private void Destruction()
-    {
-        Destroy(gameObject);
+            if (_isShare)
+            {
+                BreakDown();
+            }
+
+            Destruction();
+        }
+
+        private void BreakDown()
+        {
+            for(int i = 0; i < _numberOfFragments; i++)
+            {
+                var asteroid = Instantiate(_fragmentPrefab, transform.position, Quaternion.identity);
+
+                asteroid.SetDirection(transform.rotation.z + (Mathf.Pow(-1, i) * 15 * -1));
+            }
+        }
+
+        public void SetDirection(float angle)
+        {
+            float x = transform.position.magnitude * Mathf.Cos(Mathf.Deg2Rad * angle);
+            float y = transform.position.magnitude * Mathf.Sin(Mathf.Deg2Rad * angle);
+            _direction = new Vector3(x, y).normalized;
+        }
+
+        private void Destruction()
+        {
+            Destroy(gameObject);
+        }
     }
 }
